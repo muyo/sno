muyo/sno

commit d3f9e9e071d2dd9fd141d6521f206dfd6c6d39f4

amd64: Move CPU feature detection to internal package
- Includes a proper fix for #1;
- Fixes incorrect mask checks causing false positives on CPUs without actual support for the SIMD sets we need;
- Includes new test suite covering the detection routine;

Other code (especially os/arch specific) may also get moved to the internal package to declutter the root package.

internal/cpu_amd64.go
@@ -1,0 +1,41 @@
+package internal
+
+const cpuLacksSSE2ErrMsg = "sno: CPU does not seem to support SSE2 instructions required on amd64 platforms"
+
+func HasVectorSupport() bool {
+	// We need a highest function parameter of at least 7 since we need
+	// to check for BMI2 support as well.
+	eax, _, _, _ := cpuid(0)
+	if eax < 7 {
+		if eax < 1 {
+			panic(cpuLacksSSE2ErrMsg)
+		}
+
+		return false
+	}
+
+	_, _, ecx, edx := cpuid(1)
+	if (edx & (1 << 26)) == 0 {
+		panic(cpuLacksSSE2ErrMsg)
+	}
+
+	// c & 0x00000001 -> SSE3
+	// c & 0x00000200 -> SSSE3
+	// c & 0x00080000 -> SSE4
+	// c & 0x00100000 -> SSE4.2
+	if (ecx & 0x00180201) != 0x00180201 {
+		return false
+	}
+
+	// e & 0x00000008 -> BMI1
+	// e & 0x00000100 -> BMI2
+	_, ebx, _, _ := cpuid(7)
+
+	return (ebx & 0x00000108) == 0x00000108
+}
+
+// Gets temporarily swapped out with a mock during tests.
+var cpuid = cpuidReal
+
+//go:noescape
+func cpuidReal(op uint32) (eax, ebx, ecx, edx uint32)

internal/cpu_amd64.s
@@ -1,0 +1,12 @@
+#include "textflag.h"
+#include "funcdata.h"
+
+// func cpuidReal(op uint32) (eax, ebx, ecx, edx uint32)
+TEXT ·cpuidReal(SB), NOSPLIT, $0-24
+    MOVL op+0(FP), AX
+    CPUID
+    MOVL AX, eax+8(FP)
+    MOVL BX, ebx+12(FP)
+    MOVL CX, ecx+16(FP)
+    MOVL DX, edx+20(FP)
+    RET

internal/cpu_amd64_test.go
@@ -1,0 +1,191 @@
+package internal
+
+import (
+	"testing"
+)
+
+func TestCPU(t *testing.T) {
+	t.Run("features", func(t *testing.T) {
+		t.Run("real", testCPUReal)
+		t.Run("mocked", testCPUMocked)
+	})
+}
+
+// First tests are run against the real hardware and actual cpuid instruction.
+// While we can't reliably assume the availability of the instruction sets,
+// at the very least we may catch anomalies when the highest function parameter
+// returned is not sane - or when SSE2 instructions are not available where we
+// assume they should be.
+func testCPUReal(t *testing.T) {
+	t.Run("highest-function-parameter-valid", testCPURealMFIValid)
+	t.Run("sse-has-base-set", testCPURealSSEHasBaseSet)
+	t.Run("has-vector-support-attempt", testCPURealHasVectorSupportAttempt)
+}
+
+func testCPURealMFIValid(t *testing.T) {
+	eax, _, _, _ := cpuid(0)
+	if eax < 1 {
+		t.Errorf("expected a non-zero highest function parameter, got [%d]", eax)
+	}
+}
+
+func testCPURealSSEHasBaseSet(t *testing.T) {
+	_, _, _, edx := cpuid(1)
+	if (edx & (1 << 26)) == 0 {
+		t.Error("expected the SSE2 instruction set to be available, does not appear to be")
+	}
+}
+
+func testCPURealHasVectorSupportAttempt(t *testing.T) {
+	defer func() {
+		catch(t, recover(), "")
+	}()
+
+	// Note: We don't care about the result as we can't assume to get a 'true'.
+	// We only care for this to not panic.
+	HasVectorSupport()
+}
+
+// Note: Those tests must not run in parallel to any tests that rely
+// on real hardware and the actual cpuid implementation (vide enc/dec),
+// as the cpuid function gets swapped out for mocks.
+func testCPUMocked(t *testing.T) {
+	cpuid = cpu.id
+
+	t.Run("highest-function-parameter-invalid", testCPUHasVectorSupportMFIInvalid)
+	t.Run("highest-function-parameter-too-low", testCPUHasVectorSupportMFILow)
+	t.Run("sse-lacks-base-set", testCPUHasVectorSupportSSELacksBaseSet)
+	t.Run("sse-lacks-extended-sets", testCPUHasVectorSupportSSELacksExtendedSets)
+	t.Run("bmi-lacks-sets", testCPUHasVectorSupportBMILacksSets)
+	t.Run("passes", testCPUHasVectorPasses)
+
+	// Restore real implementation.
+	cpuid = cpuidReal
+}
+
+func testCPUHasVectorSupportMFIInvalid(t *testing.T) {
+	defer func() {
+		catch(t, recover(), cpuLacksSSE2ErrMsg)
+	}()
+
+	cpu.reset()
+	cpu.eax = 0
+	expectVectorSupport(t, false)
+}
+
+func testCPUHasVectorSupportMFILow(t *testing.T) {
+	defer func() {
+		catch(t, recover(), "")
+	}()
+
+	cpu.reset()
+	cpu.eax = 6
+	expectVectorSupport(t, false)
+}
+
+func testCPUHasVectorSupportSSELacksBaseSet(t *testing.T) {
+	defer func() {
+		catch(t, recover(), cpuLacksSSE2ErrMsg)
+	}()
+
+	cpu.reset()
+	cpu.edx ^= 1 << 26 // SSE2 is featured as 1 << 26, so we simply set everything *but*.
+	expectVectorSupport(t, false)
+}
+
+func testCPUHasVectorSupportSSELacksExtendedSets(t *testing.T) {
+	defer func() {
+		catch(t, recover(), "")
+	}()
+
+	for _, c := range []struct {
+		name string
+		mask uint32
+	}{
+		{"SSE3", ^uint32(0x00000001)},
+		{"SSSE3", ^uint32(0x00000200)},
+		{"SSE4", ^uint32(0x00080000)},
+		{"SSE4.2", ^uint32(0x00100000)},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			cpu.reset()
+			cpu.ecx = c.mask
+			expectVectorSupport(t, false)
+		})
+	}
+}
+
+func testCPUHasVectorSupportBMILacksSets(t *testing.T) {
+	defer func() {
+		catch(t, recover(), "")
+	}()
+
+	for _, c := range []struct {
+		name string
+		mask uint32
+	}{
+		{"BMI1", ^uint32(0x00000008)},
+		{"BMI2", ^uint32(0x00000100)},
+	} {
+		t.Run(c.name, func(t *testing.T) {
+			cpu.reset()
+			cpu.ebx = c.mask
+			expectVectorSupport(t, false)
+		})
+	}
+}
+
+func testCPUHasVectorPasses(t *testing.T) {
+	defer func() {
+		catch(t, recover(), "")
+	}()
+
+	cpu.reset()
+	expectVectorSupport(t, true)
+}
+
+var cpu = func() *cpuMock {
+	c := &cpuMock{}
+	c.reset()
+
+	return c
+}()
+
+type cpuMock struct {
+	eax, ebx, ecx, edx uint32
+}
+
+func (c *cpuMock) reset() {
+	c.eax = 7
+	c.ebx = 0x00000108
+	c.ecx = 0x00180201
+	c.edx = 1 << 26
+}
+
+func (c *cpuMock) id(_ uint32) (eax, ebx, ecx, edx uint32) {
+	return c.eax, c.ebx, c.ecx, c.edx
+}
+
+func catch(t *testing.T, err interface{}, expected string) {
+	if expected != "" {
+		if err == nil {
+			t.Fatalf("expected a panic with message [%s]", expected)
+		}
+
+		if err != expected {
+			t.Errorf("expected a panic with message [%s], got [%s]", expected, err)
+		}
+
+		return
+	}
+
+	if err != nil {
+		t.Fatalf("expected to not panic, panicked with [%s]", err)
+	}
+}
+
+func expectVectorSupport(t *testing.T, expected bool) {
+	if actual := HasVectorSupport(); actual != expected {
+		t.Errorf("expected [%t], got [%t]", expected, actual)
+	}
+}

.travis.yml
@@ -22,7 +22,7 @@ before_install:
   - go get github.com/mattn/goveralls
 
 script:
-  - go test -v -tags=test -race -coverprofile=coverage.txt -covermode=atomic ./
+  - go test -v -tags=test -race -coverprofile=coverage.txt -covermode=atomic ./...
 
 after_success:
   - goveralls -coverprofile=coverage.txt -service=travis-ci
@@ -44,7 +44,7 @@ jobs:
       go: "1.14"
       script:
         # Go's race detector does not work on s390x
-        - go test -v -tags=test -coverprofile=coverage.txt -covermode=atomic ./
+        - go test -v -tags=test -coverprofile=coverage.txt -covermode=atomic ./...
 
   allow_failures:
     - go: "master"

encoding_amd64.go
@@ -1,51 +1,17 @@
 package sno
 
+import "github.com/muyo/sno/internal"
+
 //go:noescape
 func encode(src *ID) (dst [SizeEncoded]byte)
 
 //go:noescape
 func decode(src []byte) (dst ID)
 
-//go:noescape
-func cpuId(op uint8) (eax, ebx, ecx, edx uint32)
-
-const cpuLacksSSE2ErrMsg = "sno: CPU does not seem to support SSE2 instructions required on amd64 platforms"
-
-// One-shot to determine whether we've got SSE2 at all, and the SSE4.2 and BMI2 sets
+// One-shot to determine whether we've got SSE2 at all - and the SSE4.2 and BMI2 sets
 // that we need for the vectorized codecs.
-var hasVectorSupport = func() bool {
-	mfi, _, _, _ := cpuId(0)
-	mfi, _, _, _ = cpuId(0) // TODO(alcore) See: https://github.com/muyo/sno/pull/1
-
-	// Need an mfi of at least 7 since we need to check for BMI2 support as well.
-	if mfi < 7 {
-		if mfi < 1 {
-			// We don't even have basic sets.
-			panic(cpuLacksSSE2ErrMsg)
-		}
-
-		return false
-	}
-
-	_, _, c, d := cpuId(1)
-	// The fallbacks currently rely on SSE2 - while it's available on just about
-	// any modern AMD64 platform, *just in case* it's not, fail loudly and immediately
-	// instead of faulting on first encode/decode attempt.
-	if (d & (1 << 26)) == 0 {
-		panic(cpuLacksSSE2ErrMsg)
-	}
-
-	// c & 0x00000001 -> SSE3
-	// c & 0x00000200 -> SSSE3
-	// c & 0x00080000 -> SSE4
-	// c & 0x00100000 -> SSE4.2
-	if (c & 0x00180201) == 0 {
-		return false
-	}
-
-	// e & 0x00000008 -> BMI1
-	// e & 0x00000100 -> BMI2
-	_, e, _, _ := cpuId(7)
-
-	return (e & 0x00000108) != 0
-}()
+//
+// The fallbacks currently rely on SSE2 - while it's available on just about
+// any modern amd64 platform, *just in case* it's not, the check will fail loudly
+// and immediately (panic) instead of faulting on the first encode/decode attempt.
+var hasVectorSupport = internal.HasVectorSupport()

encoding_amd64.s
@@ -260,16 +260,3 @@ decodeFallback:
     MOVB BX, 9(DX)
 
     RET
-
-
-// func cpuId(op uint8) (eax, ebx, ecx, edx uint32)
-TEXT ·cpuId(SB), NOSPLIT, $16-24
-    XORQ CX, CX
-    MOVB op+0(FP), AX
-    CPUID
-    MOVL AX, eax+8(FP)
-    MOVL BX, ebx+12(FP)
-    MOVL CX, ecx+16(FP)
-    MOVL DX, edx+20(FP)
-
-    RET

encoding_test.go
@@ -28,13 +28,12 @@ func testEncodingDecode(t *testing.T) {
 }
 
 func runEncodingWithFallback(name string, t *testing.T, f func(t *testing.T)) {
-	if hasVectorSupport {
-		t.Run(name+"-vectorized", f)
-	} else {
+	if !hasVectorSupport {
 		t.Run(name+"-fallback", f)
 		return
 	}
 
+	t.Run(name+"-vectorized", f)
 	hasVectorSupport = false
 	t.Run(name+"-fallback", f)
 	hasVectorSupport = true

commit 064352b7c98beef2f1ca3c277feaddee74eb0647

amd64: Clear ecx before cpuid instruction
- Fixes an oversight introduced along with d3f9e9e071d2dd9fd141d6521f206dfd6c6d39f4;
- Includes some adjustments slightly flattening the CPU tests;

internal/cpu_amd64.go
@@ -27,8 +27,8 @@ func HasVectorSupport() bool {
 		return false
 	}
 
-	// e & 0x00000008 -> BMI1
-	// e & 0x00000100 -> BMI2
+	// b & 0x00000008 -> BMI1
+	// b & 0x00000100 -> BMI2
 	_, ebx, _, _ := cpuid(7)
 
 	return (ebx & 0x00000108) == 0x00000108

internal/cpu_amd64.s
@@ -3,7 +3,8 @@
 
 // func cpuidReal(op uint32) (eax, ebx, ecx, edx uint32)
 TEXT ·cpuidReal(SB), NOSPLIT, $0-24
-    MOVL op+0(FP), AX
+	MOVL op+0(FP), AX
+    XORQ CX, CX
     CPUID
     MOVL AX, eax+8(FP)
     MOVL BX, ebx+12(FP)

internal/cpu_amd64_test.go
@@ -5,10 +5,8 @@ import (
 )
 
 func testCPU(t *testing.T) {
-	t.Run("features", func(t *testing.T) {
-		t.Run("real", testCPUReal)
-		t.Run("mocked", testCPUMocked)
-	})
+	t.Run("real", testCPUReal)
+	t.Run("mocked", testCPUMocked)
 }
 
 // First tests are run against the real hardware and actual cpuid instruction.
@@ -18,7 +16,7 @@ func testCPU(t *testing.T) {
 // assume they should be.
 func testCPUReal(t *testing.T) {
 	t.Run("highest-function-parameter-valid", testCPURealMFIValid)
-	t.Run("sse-has-base-set", testCPURealSSEHasBaseSet)
+	t.Run("has-base-set", testCPURealHasBaseSet)
 	t.Run("has-vector-support-attempt", testCPURealHasVectorSupportAttempt)
 }
 
@@ -29,7 +27,7 @@ func testCPURealMFIValid(t *testing.T) {
 	}
 }
 
-func testCPURealSSEHasBaseSet(t *testing.T) {
+func testCPURealHasBaseSet(t *testing.T) {
 	_, _, _, edx := cpuid(1)
 	if (edx & (1 << 26)) == 0 {
 		t.Error("expected the SSE2 instruction set to be available, does not appear to be")
@@ -54,9 +52,8 @@ func testCPUMocked(t *testing.T) {
 
 	t.Run("highest-function-parameter-invalid", testCPUHasVectorSupportMFIInvalid)
 	t.Run("highest-function-parameter-too-low", testCPUHasVectorSupportMFILow)
-	t.Run("sse-lacks-base-set", testCPUHasVectorSupportSSELacksBaseSet)
-	t.Run("sse-lacks-extended-sets", testCPUHasVectorSupportSSELacksExtendedSets)
-	t.Run("bmi-lacks-sets", testCPUHasVectorSupportBMILacksSets)
+	t.Run("lacks-base-set", testCPUHasVectorSupportLacksBaseSet)
+	t.Run("lacks-extended-sets", testCPUHasVectorSupportLacksExtendedSets)
 	t.Run("passes", testCPUHasVectorPasses)
 
 	// Restore real implementation.
@@ -83,7 +80,7 @@ func testCPUHasVectorSupportMFILow(t *testing.T) {
 	expectVectorSupport(t, false)
 }
 
-func testCPUHasVectorSupportSSELacksBaseSet(t *testing.T) {
+func testCPUHasVectorSupportLacksBaseSet(t *testing.T) {
 	defer func() {
 		catch(t, recover(), cpuLacksSSE2ErrMsg)
 	}()
@@ -93,43 +90,33 @@ func testCPUHasVectorSupportSSELacksBaseSet(t *testing.T) {
 	expectVectorSupport(t, false)
 }
 
-func testCPUHasVectorSupportSSELacksExtendedSets(t *testing.T) {
+func testCPUHasVectorSupportLacksExtendedSets(t *testing.T) {
 	defer func() {
 		catch(t, recover(), "")
 	}()
 
 	for _, c := range []struct {
 		name string
-		mask uint32
+		ebx  uint32
+		ecx  uint32
 	}{
-		{"SSE3", ^uint32(0x00000001)},
-		{"SSSE3", ^uint32(0x00000200)},
-		{"SSE4", ^uint32(0x00080000)},
-		{"SSE4.2", ^uint32(0x00100000)},
+		{"SSE3", 0, ^uint32(0x00000001)},
+		{"SSSE3", 0, ^uint32(0x00000200)},
+		{"SSE4", 0, ^uint32(0x00080000)},
+		{"SSE4.2", 0, ^uint32(0x00100000)},
+		{"BMI1", ^uint32(0x00000008), 0},
+		{"BMI2", ^uint32(0x00000100), 0},
 	} {
 		t.Run(c.name, func(t *testing.T) {
 			cpu.reset()
-			cpu.ecx = c.mask
-			expectVectorSupport(t, false)
-		})
-	}
-}
+			if c.ebx != 0 {
+				cpu.ebx = c.ebx
+			}
 
-func testCPUHasVectorSupportBMILacksSets(t *testing.T) {
-	defer func() {
-		catch(t, recover(), "")
-	}()
+			if c.ecx != 0 {
+				cpu.ecx = c.ecx
+			}
 
-	for _, c := range []struct {
-		name string
-		mask uint32
-	}{
-		{"BMI1", ^uint32(0x00000008)},
-		{"BMI2", ^uint32(0x00000100)},
-	} {
-		t.Run(c.name, func(t *testing.T) {
-			cpu.reset()
-			cpu.ebx = c.mask
 			expectVectorSupport(t, false)
 		})
 	}